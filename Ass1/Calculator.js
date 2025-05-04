fe38e8c5a4923657672f9029e86b94cd8a88eb86
Atharva-Shelke <97449247+Atharva-Shelke@users.noreply.github.com>
2025-05-05 01:59:42 +0530

    Update Calculator.js

--- Ass1/Calculator.js
+++ Ass1/Calculator.js
@@ -1,39 +1,85 @@
 function display(value){
-    calculator.screen.value+=value;
+	calculator.screen.value+=value;
+	click=true;
+	if(i==1){
+		i=0;
+	}
+}
+var click;
+
+function display1(value){
+	if(value=="-"){
+		calculator.screen.value+=value;
+		click = false;
+	}
+	if(click){
+		(i == 1 && value == "-") || i == 0 ? (calculator.screen.value += value, click = false) : null;
+	}
 }
 
 var i = 0;
 function brackets(){
-if(i == 0){
-calculator.screen.value += '(';
-i = 1;
-}
-else if(i == 1){
-calculator.screen.value += ')';
-i = 0;
-}
+	var x = calculator.screen.value;
+	var y = x.slice(-1);
+	if(i == 0 && (y == "+" || y == "-" || y == "x" || y == "/" || y == "%" || y == "")){
+		calculator.screen.value += '(';
+		i = 1;
+	}
+	else if((i == 1 && !(y == "+" || y == "-" || y == "x" || y == "/" || y == "%" || y == "(")) ||
+	(i == 0 && !(y == "+" || y == "-" || y == "x" || y == "/" || y == "%" || y == "(" || x.length == 1)))
+	{
+		calculator.screen.value += ')';
+		i = 0;
+	}
+	click=true;
 }
 
 function backspace(){
-var x = calculator.screen.value;
-if(x.length > 0){
-x = x.substring(0, x.length-1);
-calculator.screen.value = x;
-}
+	var x = calculator.screen.value;
+	if(x.length > 0){
+		x = x.substring(0, x.length-1);
+		calculator.screen.value = x;
+	}
+	click=true;
 }
 
 function Cbtn(){
-alert("Alert : Clearing Screen");
-calculator.screen.value="";
+	i=0;
+    document.getElementById('clearScreen').style.display = 'block';
+	calculator.screen.value="";
+	disableForm();
 }
 
 function equalTo(){
-    calculator.screen.value= eval(calculator.screen.value);
+	var displayVal=calculator.screen.value.replaceAll('x','*');
+    calculator.screen.value= eval(displayVal);
 }
 
 function val(){
     var y= calculator.screen.value;
     if(y=='undefined'){
-    alert("No Input");
+		document.getElementById('noInput').style.display = 'block';
+		calculator.screen.value="";
+		disableForm();
     }
-}   
+}
+
+function restrictKeyboard(event) {
+  event.preventDefault();
+}
+
+function disableForm() {
+  const elements = document.getElementById('calculator').elements;
+  for (let i = 0; i < elements.length; i++) {
+	if(elements[i].name != 'clrScrn' && elements[i].name != "noIn"){
+		elements[i].disabled = true;
+	}
+  }
+}
+
+function enableForm() {
+  const elements = document.getElementById('calculator').elements;
+  for (let i = 0; i < elements.length; i++) {
+		elements[i].disabled = false;
+  }
+}
